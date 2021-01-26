8ea73337b0c7fceaa9ad1edac3643d6a0533dda5
andrew beatrice <abeatrice.mail@gmail.com>
2021-01-25 18:13:56 -0800

    init

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="text-white bg-gray-800 p-5">
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/recipes" class="ml-5">Recipes</nuxt-link>
+    </div>
+    <Nuxt class="mt-5 mx-5" />
+  </div>
+</template>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>Recipes</h1>
+    <ul class="mt-5">
+      <li v-for="recipe in recipes" :key="recipe.ID">{{recipe.Name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      recipes: []
+    }
+  },
+  mounted() {
+    this.$axios.$get("http://127.0.0.1:3000/recipes")
+      .then(res => this.recipes = res.data)
+  },
+}
+</script>
